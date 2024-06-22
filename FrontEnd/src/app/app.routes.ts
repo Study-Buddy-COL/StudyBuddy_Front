741b037ab47b3282ccea84d0c854f105f9b1e4c6
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-06-22 13:31:08 -0500

    Added space repetition route

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SpaceRepetitionComponent } from './modules/space-repetition/space-repetition.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'spaceRepetition', component: SpaceRepetitionComponent},
+];
