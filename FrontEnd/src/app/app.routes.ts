0eed4ff417a7385de73a767d94c43f6ce2a59a2f
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-06-22 13:46:20 -0500

    Added router outlet config

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SpaceRepetitionComponent } from './modules/space-repetition/space-repetition.component';
+import { MainComponent } from './modules/landing/components/main/main.component';
 
 export const routes: Routes = [
-    {path: 'spaceRepetition', component: SpaceRepetitionComponent},
+    {path: '', component: MainComponent}, // Landing - Main
+    {path: 'spaceRepetition', component: SpaceRepetitionComponent}, // Space repetition
 ];
