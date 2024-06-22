532097e9c1e89d30f67b33bea0b314a9a0b8d719
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-06-22 13:29:16 -0500

    Generate space-repetition component

---
+++ FrontEnd/src/app/modules/space-repetition/space-repetition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-space-repetition',
+  standalone: true,
+  imports: [],
+  templateUrl: './space-repetition.component.html',
+  styleUrl: './space-repetition.component.css'
+})
+export class SpaceRepetitionComponent {
+
+}
