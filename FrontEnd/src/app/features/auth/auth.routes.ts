52587bc4b519bca9d641059ab7d3efa41f5a7224
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2024-11-30 03:08:52 -0500

    SignIn (Not responsive)

--- FrontEnd/src/app/features/auth/auth.routes.ts
+++ FrontEnd/src/app/features/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from "@angular/router";
+import { SignInComponent } from "./pages/login-sign-up/sign-in.component";
 
 export const AUTH_ROUTES :Routes = [
-  { path: '', }
+  { path: '', component: SignInComponent}
 ];
 
