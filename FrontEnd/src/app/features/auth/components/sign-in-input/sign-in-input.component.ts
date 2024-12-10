8026fe441d019bc725e6faf14920886162b73f98
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-12-09 22:31:42 -0500

    My last free commit :(

---
+++ FrontEnd/src/app/features/auth/components/sign-in-input/sign-in-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-in-input.component.html',
+  styleUrl: './sign-in-input.component.css'
+})
+export class SignInInputComponent {
+
+}
--- FrontEnd/src/app/features/auth/pages/sign-in/sign-in.component.ts
+++ FrontEnd/src/app/features/auth/pages/sign-in/sign-in.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';  // Importa RouterModule
+import { RouterModule } from '@angular/router';
+import {SignInInputComponent} from "../../components/sign-in-input/sign-in-input.component";  // Importa RouterModule
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SignInInputComponent],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
